c2a8aca6c0a6184435116c3592933d77b069410d
Rodrigo Rodrigues <rodrigo.rodrigues@compasso.com.br>
2022-08-29 22:30:54 -0300

    setup

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+    <ul>
+
+    </ul>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            storedResources: [
+                {
+                    id: '',
+                    title: '',
+                    description: '',
+                    link: '',
+                },
+                {
+                    id: '',
+                    title: '',
+                    description: '',
+                    link: '',
+                }
+            ]
+        };
+    },
+}
+</script>
+
+<style>
+
+</style>
