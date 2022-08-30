7db7548e91fddab3781c3b8de435c48b3425eed9
Rodrigo Rodrigues <rodrigo.rodrigues@compasso.com.br>
2022-08-29 22:54:12 -0300

    component LearningResource

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,38 @@
 <template>
     <ul>
-
+        <learning-resource 
+            v-for="res in storedResources" 
+            :key="res.id"
+            :title="res.title"
+            :description="res.description"
+            :link="res.link"
+        >
+        </learning-resource>
     </ul>
 </template>
 
 <script>
+import LearningResource from './componets/learning-resources/LearningResource.vue';
+
 export default {
+    components: {
+        LearningResource,
+    },
+
     data() {
         return {
             storedResources: [
                 {
-                    id: '',
-                    title: '',
-                    description: '',
-                    link: '',
+                    id: 'sql',
+                    title: 'SQL Plus',
+                    description: 'Lean SQL Plus',
+                    link: 'https://www.oracle.com/br/database/technologies/sqlplus-cloud.html',
                 },
                 {
-                    id: '',
-                    title: '',
-                    description: '',
-                    link: '',
+                    id: 'joins',
+                    title: 'Joins',
+                    description: 'Lean Joins',
+                    link: 'https://www.devmedia.com.br/clausulas-inner-join-left-join-e-right-join-no-sql-server/18930',
                 }
             ]
         };
